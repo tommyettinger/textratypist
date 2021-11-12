a654f195aece8608dd1722253722bb5963f135d0
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-11-12 12:39:49 -0800

    Mark this down for later. Shrinking works, widening doesn't.

--- src/main/java/com/github/tommyettinger/textra/Font.java
+++ src/main/java/com/github/tommyettinger/textra/Font.java
@@ -2275,10 +2275,10 @@ public class Font implements Disposable {
                         else
                             next = changing.getLine(ln+1);
                         int nextSize = next.glyphs.size;
-                        long[] arr = next.glyphs.ensureCapacity(i - cutoff);
-                        System.arraycopy(arr, 0, arr, 1 + i - cutoff, nextSize);
-                        System.arraycopy(glyphs.items, cutoff, arr, 0, 1 + i - cutoff);
-                        next.glyphs.size += 1 + i - cutoff;
+                        long[] arr = next.glyphs.ensureCapacity(glyphs.size - cutoff);
+                        System.arraycopy(arr, 0, arr, glyphs.size - cutoff, nextSize);
+                        System.arraycopy(glyphs.items, cutoff, arr, 0, glyphs.size - cutoff);
+                        next.glyphs.size += glyphs.size - cutoff;
                         glyphs.truncate(cutoff);
                         break;
                     }
