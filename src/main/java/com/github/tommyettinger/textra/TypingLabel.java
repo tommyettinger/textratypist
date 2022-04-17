90fcea26d5e446a62f2561edd6fb519706b726c0
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-04-17 02:46:27 -0700

    It looks like we do need to re-parse tokens.
    Former-commit-id: e542ee16e7b6a3ab14d613fc701e5420a52ab151

--- src/main/java/com/github/tommyettinger/textra/TypingLabel.java
+++ src/main/java/com/github/tommyettinger/textra/TypingLabel.java
@@ -576,7 +576,7 @@ public class TypingLabel extends TextraLabel {
         }
         if (wrap) {
             font.regenerateLayout(workingLayout);
-//            parseTokens();
+            parseTokens();
         }
         else {
             for (Line ln : workingLayout.lines) {
