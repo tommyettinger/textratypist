17f0bc3596193b29f7099017d7b51fa55110ac38
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-04-16 23:04:10 -0700

    Docs and extra constructors for TextraLabel.

--- src/main/java/com/github/tommyettinger/textra/TextraLabel.java
+++ src/main/java/com/github/tommyettinger/textra/TextraLabel.java
@@ -27,8 +27,6 @@ import com.badlogic.gdx.utils.Align;
 /**
  * A scene2d.ui Widget that displays text using a {@link Font} rather than a libGDX BitmapFont. This supports being
  * laid out in a Table just like the typical Label.
- * <br>
- * ...Maybe not quite ready for prime-time yet. This could use some more features for ease-of-use.
  */
 public class TextraLabel extends Widget {
     public Layout layout;
@@ -36,41 +34,95 @@ public class TextraLabel extends Widget {
     public int align = Align.left;
     public boolean wrap = false;
     public String storedText;
+
+    /**
+     * Creates a TextraLabel that uses the default libGDX font (lsans-15 in the current version) with white color.
+     */
     public TextraLabel(){
         layout = Layout.POOL.obtain();
         font = new Font(new BitmapFont(), Font.DistanceFieldType.STANDARD, 0, 0, 0, 0);
     }
+
+    /**
+     * Creates a TextraLabel with the given text (which may be multi-line) and using the specified Skin's LabelStyle.
+     * @param text the text to use; may be multi-line, but will default to not wrapping
+     * @param skin the default Label.LabelStyle will be obtained from this and used
+     */
     public TextraLabel(String text, Skin skin) {
         this(text, skin.get(Label.LabelStyle.class));
     }
 
+    /**
+     * Creates a TextraLabel with the given text (which may be multi-line) and using the specified style from the given
+     * Skin.
+     * @param text the text to use; may be multi-line, but will default to not wrapping
+     * @param skin the default Label.LabelStyle will be obtained from this and used
+     * @param styleName the name of a Label.LabelStyle to use from the Skin
+     */
     public TextraLabel(String text, Skin skin, String styleName) {
         this(text, skin.get(styleName, Label.LabelStyle.class));
     }
+    /**
+     * Creates a TextraLabel with the given text (which may be multi-line), using the specified style from the given
+     * Skin, with the default Color overridden by the given one.
+     * @param text the text to use; may be multi-line, but will default to not wrapping
+     * @param skin the default Label.LabelStyle will be obtained from this and used
+     * @param styleName the name of a Label.LabelStyle to use from the Skin
+     * @param color the color to use for the font when unspecified (at the start and when reset)
+     */
+    public TextraLabel(String text, Skin skin, String styleName, Color color) {
+        this(text, new Font(skin.get(styleName, Label.LabelStyle.class).font, Font.DistanceFieldType.STANDARD, 0, 0, 0, 0), color);
+    }
 
+    /**
+     * Creates a TextraLabel with the given text (which may be multi-line), using the specified style from the given
+     * Skin, with the default Color overridden by the color with the given name in the skin.
+     * @param text the text to use; may be multi-line, but will default to not wrapping
+     * @param skin the default Label.LabelStyle will be obtained from this and used
+     * @param styleName the name of a Label.LabelStyle to use from the Skin
+     * @param colorName the name in the skin of the color to use for the font when unspecified (at the start and when reset)
+     */
+    public TextraLabel(String text, Skin skin, String styleName, String colorName) {
+        this(text, new Font(skin.get(styleName, Label.LabelStyle.class).font, Font.DistanceFieldType.STANDARD, 0, 0, 0, 0), skin.get(colorName, Color.class));
+    }
 
+    /**
+     * Creates a TextraLabel with the given text (which may be multi-line) and using the given style.
+     * @param text the text to use; may be multi-line, but will default to not wrapping
+     * @param style the Label.LabelStyle to use
+     */
     public TextraLabel(String text, Label.LabelStyle style) {
         font = new Font(style.font, Font.DistanceFieldType.STANDARD, 0, 0, 0, 0);
         layout = Layout.POOL.obtain();
         layout.setBaseColor(style.fontColor);
         storedText = text;
         font.markup(text, layout);
-//        setSize(layout.getWidth(), layout.getHeight());
     }
+
+    /**
+     * Creates a TextraLabel with the given text (which may be multi-line) and using the given Font.
+     * @param text the text to use; may be multi-line, but will default to not wrapping
+     * @param font a Font from this library, such as one obtained from {@link KnownFonts}
+     */
     public TextraLabel(String text, Font font) {
         this.font = font;
         layout = Layout.POOL.obtain();
         storedText = text;
         font.markup(text, layout);
-//        setSize(layout.getWidth(), layout.getHeight());
     }
+    /**
+     * Creates a TextraLabel with the given text (which may be multi-line), using the given Font, and using the given
+     * default color.
+     * @param text the text to use; may be multi-line, but will default to not wrapping
+     * @param font a Font from this library, such as one obtained from {@link KnownFonts}
+     * @param color the color to use for the font when unspecified (at the start and when reset)
+     */
     public TextraLabel(String text, Font font, Color color) {
         this.font = font;
         layout = Layout.POOL.obtain();
         layout.setBaseColor(color);
         storedText = text;
         font.markup(text, layout);
-//        setSize(layout.getWidth(), layout.getHeight());
     }
 
     @Override
@@ -95,10 +147,22 @@ public class TextraLabel extends Widget {
         return layout.getHeight() + font.cellHeight * 0.5f;
     }
 
+    /**
+     * Gets the current wrapping mode. When wrap is enabled,
+     * the preferred and/or min/max sizes must be used so this knows where to wrap. If wrap is disabled, lines that are
+     * too wide will just widen the size of the widget.
+     * @return whether this is currently wrapping
+     */
     public boolean isWrap() {
         return wrap;
     }
 
+    /**
+     * Sets the wrapping mode; if this changes the mode, then this invalidates the hierarchy. When wrap is enabled,
+     * the preferred and/or min/max sizes must be used so this knows where to wrap. If wrap is disabled, lines that are
+     * too wide will just widen the size of the widget.
+     * @param wrap whether to wrap or not
+     */
     public void setWrap(boolean wrap) {
         if(this.wrap != (this.wrap = wrap))
             invalidateHierarchy();
