d2314887fb2dbeade0c1dc4043bac3bbd991f545
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-04-18 21:44:49 -0700

    TextraCheckBox, initial draft.
    Former-commit-id: 2013ffe267c63c64a97846d109a7ce8708cef1c6

---
+++ src/main/java/com/github/tommyettinger/textra/TextraCheckBox.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2022 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.tommyettinger.textra;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.ui.Cell;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox.CheckBoxStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Null;
+import com.badlogic.gdx.utils.Scaling;
+
+/** A checkbox is a button that contains an image indicating the checked or unchecked state and a label.
+ * @author Nathan Sweet */
+public class TextraCheckBox extends TextraButton {
+	private Image image;
+	private Cell<?> imageCell;
+	private CheckBoxStyle style;
+
+	public TextraCheckBox(@Null String text, Skin skin) {
+		this(text, skin.get(CheckBoxStyle.class));
+	}
+
+	public TextraCheckBox(@Null String text, Skin skin, String styleName) {
+		this(text, skin.get(styleName, CheckBoxStyle.class));
+	}
+
+	public TextraCheckBox(@Null String text, CheckBoxStyle style) {
+		super(text, style);
+
+		TextraLabel label = getTextraLabel();
+		label.setAlignment(Align.left);
+
+		image = newImage();
+		image.setDrawable(style.checkboxOff);
+
+		clearChildren();
+		imageCell = add(image);
+		add(label);
+		setSize(getPrefWidth(), getPrefHeight());
+	}
+
+	protected Image newImage () {
+		return new Image((Drawable)null, Scaling.none);
+	}
+
+	public void setStyle (ButtonStyle style) {
+		if (!(style instanceof CheckBoxStyle)) throw new IllegalArgumentException("style must be a CheckBoxStyle.");
+		this.style = (CheckBoxStyle)style;
+		super.setStyle(style);
+	}
+
+	/** Returns the checkbox's style. Modifying the returned style may not have an effect until {@link #setStyle(ButtonStyle)} is
+	 * called. */
+	public CheckBoxStyle getStyle () {
+		return style;
+	}
+
+	public void draw (Batch batch, float parentAlpha) {
+		Drawable checkbox = null;
+		if (isDisabled()) {
+			if (isChecked() && style.checkboxOnDisabled != null)
+				checkbox = style.checkboxOnDisabled;
+			else
+				checkbox = style.checkboxOffDisabled;
+		}
+		if (checkbox == null) {
+			boolean over = isOver() && !isDisabled();
+			if (isChecked() && style.checkboxOn != null)
+				checkbox = over && style.checkboxOnOver != null ? style.checkboxOnOver : style.checkboxOn;
+			else if (over && style.checkboxOver != null)
+				checkbox = style.checkboxOver;
+			else
+				checkbox = style.checkboxOff;
+		}
+		image.setDrawable(checkbox);
+		super.draw(batch, parentAlpha);
+	}
+
+	public Image getImage () {
+		return image;
+	}
+
+	public Cell<?> getImageCell () {
+		return imageCell;
+	}
+}
