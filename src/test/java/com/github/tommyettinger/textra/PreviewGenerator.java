b33b9d2cfd90725c7c25d42f27ce19d56c30c461
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-07-26 20:02:39 -0700

    More fonts, with KingThings adjustments again.
    It's a nice font for sure, but the metrics on it are tricky to work with.

--- src/test/java/com/github/tommyettinger/textra/PreviewGenerator.java
+++ src/test/java/com/github/tommyettinger/textra/PreviewGenerator.java
@@ -135,7 +135,7 @@ YanoneKaffeesatz-standard.fnt has descent: -19
             ScreenUtils.clear(0.8f, 0.8f, 0.8f, 1f);
 //            ScreenUtils.clear(0.3f, 0.3f, 0.3f, 1f);
 //        layout.getLine(0).glyphs.set(0, font.markupGlyph('@', "[" + colorNames.get((int)(TimeUtils.timeSinceMillis(startTime) >>> 8) % colorNames.size) + "]"));
-            float x = 400, y = layout.getHeight();
+            float x = 400, y = layout.getHeight() - font.descent * font.scaleY;
             batch.begin();
             font.enableShader(batch);
             font.drawGlyphs(batch, layout, x, y, Align.center);
@@ -161,7 +161,7 @@ YanoneKaffeesatz-standard.fnt has descent: -19
         ScreenUtils.clear(0.8f, 0.8f, 0.8f, 1f);
 //        ScreenUtils.clear(0.3f, 0.3f, 0.3f, 1f);
 //        layout.getLine(0).glyphs.set(0, font.markupGlyph('@', "[" + colorNames.get((int)(TimeUtils.timeSinceMillis(startTime) >>> 8) % colorNames.size) + "]"));
-        float x = 400, y = layout.getHeight();
+        float x = 400, y = layout.getHeight() - fnt.descent * fnt.scaleY;
         batch.begin();
         fnt.enableShader(batch);
         fnt.drawGlyphs(batch, layout, x, y, Align.center);
