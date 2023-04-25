453188b3bb199c516b5b2e433c9bc3121071eb4a
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-04-25 14:36:04 -0700

    Start comparing specific versions for regression.
    Former-commit-id: 4ac9047f77dfc43b3a7cf417eeb29ea161874df9

---
+++ versions/v0-8-3/core/src/main/java/com/github/tommyettinger/Main.java
@@ -0,0 +1,38 @@
+package com.github.tommyettinger;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.github.tommyettinger.textra.Font;
+import com.github.tommyettinger.textra.KnownFonts;
+import com.github.tommyettinger.textra.TextraLabel;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+    Stage stage;
+    Font font;
+    @Override
+    public void create() {
+        stage = new Stage();
+        font = KnownFonts.getRobotoCondensed();
+        TextraLabel label = new TextraLabel("Texttexttextte xtext text t e x t text text text text", font);
+        label.setWrap(true);
+        label.setWidth(200);
+        label.setAlignment(Align.bottomLeft);
+        stage.addActor(label);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(Color.BLACK);
+        stage.act();
+        stage.draw();
+    }
+}
