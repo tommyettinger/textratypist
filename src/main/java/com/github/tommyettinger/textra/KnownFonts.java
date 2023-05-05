e087488afa0eab3407e505baa35ca706249bb306
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-05-04 21:55:27 -0700

    Tangerine (SDF), Yanone K. (both), Yataghan.
    Former-commit-id: 0ff792a5b4542dcff6724195a5e8167fd5eb8460

--- src/main/java/com/github/tommyettinger/textra/KnownFonts.java
+++ src/main/java/com/github/tommyettinger/textra/KnownFonts.java
@@ -1498,7 +1498,8 @@ public final class KnownFonts implements LifecycleListener {
         if (instance.tangerineSDF == null) {
             try {
                 instance.tangerineSDF = new Font(instance.prefix + "Tangerine-sdf.fnt",
-                        instance.prefix + "Tangerine-sdf.png", SDF, 0f, 0f, 0f, 0, false)
+                        instance.prefix + "Tangerine-sdf.png", SDF, 0f, 28f, 0f, 0, false)
+                        .setLineMetrics(0.0625f, 0f, 0f, 0f).setInlineImageMetrics(0f, 0f, 4f)
                         .scaleTo(48, 32).setCrispness(0.375f).setName("Tangerine (SDF)");
             } catch (Exception e) {
                 e.printStackTrace();
@@ -1572,7 +1573,7 @@ public final class KnownFonts implements LifecycleListener {
         if (instance.kaffeesatzMSDF == null) {
             try {
                 instance.kaffeesatzMSDF = new Font(instance.prefix + "YanoneKaffeesatz-msdf.fnt",
-                        instance.prefix + "YanoneKaffeesatz-msdf.png", MSDF, 0f, 8f, 0f, 0, true)
+                        instance.prefix + "YanoneKaffeesatz-msdf.png", MSDF, 0f, 20f, 0f, 0, true)
                         .scaleTo(26, 30).setCrispness(2.5f).setName("Yanone Kaffeesatz (MSDF)");
             } catch (Exception e) {
                 e.printStackTrace();
@@ -1612,7 +1613,8 @@ public final class KnownFonts implements LifecycleListener {
         if (instance.yataghanMSDF == null) {
             try {
                 instance.yataghanMSDF = new Font(instance.prefix + "Yataghan-msdf.fnt",
-                        instance.prefix + "Yataghan-msdf.png", MSDF, 0f, -32f, 0f, 0, true)
+                        instance.prefix + "Yataghan-msdf.png", MSDF, 0f, 4f, 0f, 0f, true)
+                        .setLineMetrics(0f, 0.125f, 0f, -0.4f)
                         .scaleTo(20, 32).setName("Yataghan (MSDF)");
             } catch (Exception e) {
                 e.printStackTrace();
