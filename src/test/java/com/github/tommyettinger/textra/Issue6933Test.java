90b5d92c0dd6fbfa470d446a78e4e70f28a89d93
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-09-18 17:50:16 -0700

    Test Freetype with GL32.
    It seems fine in this tiny test...
    
    
    Former-commit-id: 41ae983eb7fa98c8dfd09f109c8a5d689c57a4b0

--- src/test/java/com/github/tommyettinger/textra/Issue6933Test.java
+++ src/test/java/com/github/tommyettinger/textra/Issue6933Test.java
@@ -22,12 +22,12 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-
-import static com.badlogic.gdx.utils.Align.center;
 
 public class Issue6933Test extends ApplicationAdapter {
     FitViewport viewport;
@@ -40,11 +40,21 @@ public class Issue6933Test extends ApplicationAdapter {
         viewport.update(Gdx.graphics.getWidth(),Gdx.graphics.getHeight(),true);
         stage = new Stage(viewport);
 
-        BitmapFont font = new BitmapFont();
+//        BitmapFont font = new BitmapFont();
+//        label = new Label("Checking for updates...", new Label.LabelStyle(font, Color.WHITE));
+//        label.setAlignment(Align.center);
+//        label.setFillParent(true);
+//        stage.addActor(label);
 
-        label = new Label("Checking for updates...", new Label.LabelStyle(font, Color.WHITE));
-        label.setAlignment(center);
-        label.setFillParent(true);
+        FreeTypeFontGenerator freeTypeFontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("LeagueSpartan-Bold.otf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.size = 60;
+        BitmapFont bitmapFont = freeTypeFontGenerator.generateFont(parameter);
+        Label.LabelStyle labelStyle = new Label.LabelStyle(bitmapFont, Color.WHITE);
+        this.label = new Label("FPS: " + Gdx.graphics.getFramesPerSecond(), labelStyle);
+        label.setSize(100f, 60f);
+        label.setAlignment(Align.left, Align.bottom);
+        label.setPosition(100f, Gdx.graphics.getHeight() - 60f);
         stage.addActor(label);
     }
 
@@ -79,8 +89,9 @@ public class Issue6933Test extends ApplicationAdapter {
         config.setTitle("TextraLabel UI test");
         config.setWindowedMode(600, 480);
         config.disableAudio(true);
-		config.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
+        config.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
         config.useVsync(true);
+        config.setOpenGLEmulation(Lwjgl3ApplicationConfiguration.GLEmulation.GL32, 3, 2);
         new Lwjgl3Application(new Issue6933Test(), config);
     }
 
