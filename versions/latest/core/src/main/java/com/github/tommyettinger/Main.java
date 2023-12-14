06a4e75bc9adcfd8f759eb20ffa4aea9b90330f8
Tommy Ettinger <tommy.ettinger@gmail.com>
2023-12-14 02:58:45 -0800

    Apparently, it was a (now fixed) TeaVM bug.
    Thanks to Xpe for figuring out that even the b8 version was a bit behind the latest TeaVM.
    
    
    Former-commit-id: 4370b46b46405a3fb662ba9fad826f64b9e7e413

--- versions/latest/core/src/main/java/com/github/tommyettinger/Main.java
+++ versions/latest/core/src/main/java/com/github/tommyettinger/Main.java
@@ -34,10 +34,10 @@ public class Main extends ApplicationAdapter {
         table.add(label).size(200, 100).row();
 
         BitmapFont bmfont = new BitmapFont(Gdx.files.internal("RobotoCondensed-standard.fnt"));
-        bmfont.getData().markupEnabled = false;
-        Label s2dLabel = new Label("I can do it!", new Label.LabelStyle(bmfont, Color.WHITE));
-//        bmfont.getData().markupEnabled = true;
-//        Label s2dLabel = new Label("I [SKY]can [ROYAL]do[] it!", new Label.LabelStyle(bmfont, Color.WHITE));
+//        bmfont.getData().markupEnabled = false;
+//        Label s2dLabel = new Label("I can do it!", new Label.LabelStyle(bmfont, Color.WHITE));
+        bmfont.getData().markupEnabled = true;
+        Label s2dLabel = new Label("I [SKY]can [ROYAL]do[] it!", new Label.LabelStyle(bmfont, Color.WHITE));
         s2dLabel.setAlignment(Align.bottom);
         table.add(s2dLabel).bottom();
 
