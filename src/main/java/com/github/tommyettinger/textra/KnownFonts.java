0b6c95a20edcc765aaf2e6f41427faff54737747
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-02-16 23:20:54 -0800

    Correct Iosevka-Slab-sdf and metrics.
    Former-commit-id: 36bcc139f43324315b52bc8682d1871d88733b3d

--- src/main/java/com/github/tommyettinger/textra/KnownFonts.java
+++ src/main/java/com/github/tommyettinger/textra/KnownFonts.java
@@ -1027,7 +1027,9 @@ public final class KnownFonts implements LifecycleListener {
                 // This should be OK now that this uses the box-drawing underline.
                 instance.iosevkaSDF = new Font(instance.prefix + "Iosevka-sdf.fnt",
                         instance.prefix + "Iosevka-sdf.png", SDF, 0f, 0f, 0f, 0f, false)
-                        .setLineMetrics(0.25f, -0.125f, 0f, -0.4f).setInlineImageMetrics(8f, 12f, 12f)
+                        .setUnderlineMetrics(-0.2f, -0.1f, 0f, -0.4f)
+                        .setStrikethroughMetrics(-0.2f, 0.125f, 0f, -0.4f)
+                        .setInlineImageMetrics(-8f, 12f, 12f)
                         .setCrispness(0.75f).scaleTo(12, 26).fitCell(10, 25, false)
                         .setName("Iosevka (SDF)");
             } catch (Exception e) {
@@ -1159,8 +1161,10 @@ public final class KnownFonts implements LifecycleListener {
                 // NOTE: If the .fnt file is changed, the manual adjustment to '_' (id=95) will be lost. yoffset was changed to 4.
                 // This might be OK now that this uses the box-drawing underline.
                 instance.iosevkaSlabSDF = new Font(instance.prefix + "Iosevka-Slab-sdf.fnt",
-                        instance.prefix + "Iosevka-Slab-sdf.png", SDF, 2f, 0f, -2f, -2f, false)
-                        .setLineMetrics(0.25f, -0.125f, 0f, -0.4f).setInlineImageMetrics(8f, 12f, 12f)
+                        instance.prefix + "Iosevka-Slab-sdf.png", SDF, 0f, 0f, 0f, 0f, false)
+                        .setUnderlineMetrics(-0.2f, -0.1f, 0f, -0.4f)
+                        .setStrikethroughMetrics(-0.2f, 0.125f, 0f, -0.4f)
+                        .setInlineImageMetrics(-8f, 12f, 12f)
                         .setCrispness(0.75f).scaleTo(12, 26).fitCell(10, 25, false)
                         .setName("Iosevka Slab (SDF)");
             } catch (Exception e) {
