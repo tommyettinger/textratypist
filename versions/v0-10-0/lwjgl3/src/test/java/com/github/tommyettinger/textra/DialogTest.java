52344798e3105f3f83ce7af7238fee5ba244ed6e
Tommy Ettinger <tommy.ettinger@gmail.com>
2024-05-19 20:08:46 -0700

    Verify that DialogTest actually looks like that.
    ... not good... but accurate...
    
    
    Former-commit-id: 6331ef40c03a10c7ec8923c8cc227bb4ac6dd5be

---
+++ versions/v0-10-0/lwjgl3/src/test/java/com/github/tommyettinger/textra/DialogTest.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright (c) 2023 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.tommyettinger.textra;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Window;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class DialogTest extends ApplicationAdapter {
+    ScreenViewport viewport;
+    Stage stage;
+    TextraDialog dialog;
+    @Override
+    public void create() {
+        viewport = new ScreenViewport();
+        viewport.update(Gdx.graphics.getWidth(),Gdx.graphics.getHeight(),true);
+        stage = new Stage(viewport);
+        Font gentium = KnownFonts.getGentium();
+        Window.WindowStyle style = new Window.WindowStyle();
+        style.background = new TextureRegionDrawable(gentium.mapping.get(gentium.solidBlock)).tint(Color.MAROON);
+        dialog = new TextraDialog("", style, gentium);
+        stage.setDebugAll(true);
+
+        dialog.getButtonTable().clear();
+        dialog.getContentTable().clear();
+        dialog.clearListeners();
+        TextraButton ok = new TextraButton("OK", new TextButton.TextButtonStyle(), gentium);
+        ok.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                dialog.hide();
+            }
+        });
+//        TextraLabel tl = new TextraLabel(
+//                "{GRADIENT=CYAN;WHITE;1;1}Come on... The Magical Mystery Tour!{ENDGRADIENT}\n" +
+//                        "The magical mystery tour:\nIs coming\nTo take you away\nDying to take you away!\nTake you,\nToday...",
+//                gentium);
+
+        ok.setVisible(false);
+        TypingLabel tl = new TypingLabel(
+                "{GRADIENT=CYAN;WHITE;1;1}Come on... The Magical Mystery Tour!{ENDGRADIENT}\n" +
+                        "The magical mystery tour:\nIs coming\nTo take you away\nDying to take you away!\nTake you,\nToday...",
+                gentium);
+        tl.setTypingListener(new TypingAdapter() {
+            @Override
+            public void end() {
+                ok.setVisible(true);
+            }
+        });
+        tl.setWrap(true);
+
+        // This was necessary so the Dialog knew what height the TypingLabel actually was.
+        // It should be automatically called by any getPrefHeight() or getPrefWidth() calls if not parsed already; that
+        // is, it will be automatically called when its size must actually be known.
+//        tl.parseTokens();
+
+        dialog.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                tl.skipToTheEnd();
+                super.clicked(event, x, y);
+            }
+        });
+
+        // This was necessary in versions 7.5 through 7.7, and shouldn't be needed later. It can't hurt, though.
+        dialog.getContentTable().clear();
+
+        dialog.getButtonTable().add(ok).width(240f);
+        dialog.getContentTable().add(tl).width(250f);
+        dialog.setKeepWithinStage(true);
+        dialog.show(stage);
+
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(Color.DARK_GRAY);
+
+        stage.act();
+        stage.draw();
+//        System.out.println(typingLabel.workingLayout.lines());
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+        // Invoked when your application is paused.
+    }
+
+    @Override
+    public void resume() {
+        // Invoked when your application is resumed after pause.
+    }
+
+    @Override
+    public void dispose() {
+        // Destroy screen's assets here.
+    }
+    public static void main(String[] args){
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("TT Dialog test");
+        config.setWindowedMode(1000, 800);
+        config.disableAudio(true);
+        config.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
+        config.useVsync(true);
+        new Lwjgl3Application(new DialogTest(), config);
+    }
+
+}
