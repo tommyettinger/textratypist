3cfd6cbb6fd75d8933cfe3cfc737a5f846a72f60
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-01-30 22:43:07 -0800

    Using the newest commit lets .fnt load on TeaVM.

--- versions/latest/core/src/main/java/com/github/tommyettinger/Main.java
+++ versions/latest/core/src/main/java/com/github/tommyettinger/Main.java
@@ -32,24 +32,24 @@ public class Main extends ApplicationAdapter {
 [CASE] UPPER_CASE_LETTERS.cardinality(): 1122
 [CASE] isLowerCase() total: 1627
 [CASE] isUpperCase() total: 1164
-[CASE] toLow.cardinality(): 1159
-[CASE] toUp .cardinality(): 1158
+[CASE] toLow.cardinality(): 1168
+[CASE] toUp .cardinality(): 1185
 
         GWT:
 CASE: LOWER_CASE_LETTERS.cardinality(): 1438
 CASE: UPPER_CASE_LETTERS.cardinality(): 1122
 CASE: isLowerCase() total: 26
 CASE: isUpperCase() total: 26
-CASE: toLow.cardinality(): 1164
-CASE: toUp .cardinality(): 1137
+CASE: toLow.cardinality(): 1173
+CASE: toUp .cardinality(): 1265
 
         TeaVM:
 CASE: LOWER_CASE_LETTERS.cardinality(): 1438
 CASE: UPPER_CASE_LETTERS.cardinality(): 1122
 CASE: isLowerCase() total: 1448
 CASE: isUpperCase() total: 1132
-CASE: toLow.cardinality(): 1210
-CASE: toUp .cardinality(): 1200
+CASE: toLow.cardinality(): 1228
+CASE: toUp .cardinality(): 1228
          */
 
 
@@ -59,14 +59,14 @@ CASE: toUp .cardinality(): 1200
         int isLow = 0, isUp = 0, to;
         BitSet toLow = new BitSet(65536);
         BitSet toUp = new BitSet(65536);
-        for (int i = 0; i < 65536; i++){
+        for (int i = 0; i < 65536; i++) {
 //        for (int i = letters.nextSetBit(0); i >= 0; i = letters.nextSetBit(i+1)){
             if(Character.isLowerCase((char)i)) isLow++;
             if(Character.isUpperCase((char)i)) isUp ++;
             to = Character.toLowerCase((char)i);
-            if(to != (char)i) toLow.set(to);
+            if(to != (char)i) toLow.set(i);
             to = Character.toUpperCase((char)i);
-            if(to != (char)i) toUp .set(to);
+            if(to != (char)i) toUp .set(i);
         }
         Gdx.app.log("CASE", "isLowerCase() total: " + isLow);
         Gdx.app.log("CASE", "isUpperCase() total: " + isUp );
@@ -74,7 +74,8 @@ CASE: toUp .cardinality(): 1200
         Gdx.app.log("CASE", "toUp .cardinality(): " + toUp .cardinality());
         stage = new Stage();
 //        FWSkin skin = new FWSkin(Gdx.files.internal("stripped/stripped.json"), new TextureAtlas("stripped/stripped.atlas"));
-        FWSkin skin = new FWSkin(Gdx.files.internal("changa.json"), new TextureAtlas("changa.atlas"));
+//        FWSkin skin = new FWSkin(Gdx.files.internal("changa.json"), new TextureAtlas("changa.atlas"));
+        FWSkin skin = new FWSkin(Gdx.files.internal("changa-old.json"), new TextureAtlas("changa-old.atlas"));
         BitmapFont bmfont = skin.getFont("one-50");
 //        BitmapFont bmfont = new BitmapFont(Gdx.files.internal("one-50.fnt"));
         bmfont.getData().markupEnabled = true;
