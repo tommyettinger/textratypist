ce21bdf7456fd675b4a3c8b4c7e62a1d11991536
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-08-26 20:43:15 -0700

    A nice, simple logo for jdkgdxds.

---
+++ src/test/java/com/github/tommyettinger/textra/JdkgdxdsLogoGenerator.java
@@ -0,0 +1,95 @@
+package com.github.tommyettinger.textra;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.PixmapIO;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import java.nio.ByteBuffer;
+
+public class JdkgdxdsLogoGenerator extends ApplicationAdapter {
+
+    private static final String LOGO_NAME = "jdkgdxds";
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 200;
+
+    SpriteBatch batch;
+    Viewport viewport;
+    Layout layout = new Layout().setTargetWidth(WIDTH);
+
+    public static void main(String[] args){
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Logo Generator");
+        config.setWindowedMode(WIDTH, HEIGHT);
+        config.disableAudio(true);
+        ShaderProgram.prependVertexCode = "#version 110\n";
+        ShaderProgram.prependFragmentCode = "#version 110\n";
+        config.setForegroundFPS(1);
+        config.useVsync(true);
+        new Lwjgl3Application(new JdkgdxdsLogoGenerator(), config);
+    }
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        viewport = new StretchViewport(WIDTH, HEIGHT);
+
+        Font font = KnownFonts.getCozette();
+        font.originalCellHeight = 13;
+        font.cellHeight = 13;
+        font.scale(5);
+
+
+        final String text = "[#999]" +
+                "┌───┬───┬──┐\n" +
+                "│[#18b]jdk[#999]│[#e74a45]gdx[#999]│[#4a5]ds[#999]│\n" +
+                "└───┴───┴──┘";
+//                "┌───┬───┬──┐\n" +
+//                "│jdk│gdx│ds│\n" +
+//                "└───┴───┴──┘";
+//                "┌───┐┌───┐┌──┐\n" +
+//                "│jdk││gdx││ds│\n" +
+//                "└───┘└───┘└──┘";
+
+        Gdx.files.local("out/").mkdirs();
+        Color baseColor = font.getDistanceField() == Font.DistanceFieldType.SDF_OUTLINE ? Color.WHITE : Color.DARK_GRAY;
+        viewport.update(Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getBackBufferHeight(), true);
+        font.resizeDistanceField(Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getBackBufferHeight(), viewport);
+        layout.setBaseColor(baseColor);
+        layout.setMaxLines(20);
+        font.markup(text, layout);
+        ScreenUtils.clear(0.1f, 0.1f, 0.1f, 1f);
+        float x = 6 * 7 * 5 - 18;
+        float y = 12 * 3 * 5;
+        batch.begin();
+        font.enableShader(batch);
+        font.resizeDistanceField(Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getBackBufferHeight(), viewport);
+        font.drawGlyphs(batch, layout, x, y, Align.center);
+        batch.end();
+
+        // Modified Pixmap.createFromFrameBuffer() code that uses RGB instead of RGBA
+        Gdx.gl.glPixelStorei(GL20.GL_PACK_ALIGNMENT, 1);
+        final Pixmap pm = new Pixmap(Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getBackBufferHeight(), Pixmap.Format.RGB888);
+        ByteBuffer pixels = pm.getPixels();
+        Gdx.gl.glReadPixels(0, 0, Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getBackBufferHeight(), GL20.GL_RGB, GL20.GL_UNSIGNED_BYTE, pixels);
+        // End Pixmap.createFromFrameBuffer() modified code
+
+//        Pixmap pm = Pixmap.createFromFrameBuffer(0, 0, Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getBackBufferHeight());
+        PixmapIO.writePNG(Gdx.files.local("out/logos/" + LOGO_NAME + ".png"), pm, 2, true);
+        Gdx.app.exit();
+    }
+
+    @Override
+    public void render() {
+    }
+}
