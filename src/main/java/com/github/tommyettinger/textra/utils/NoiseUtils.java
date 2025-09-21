2c251650fbba72402400a4285454867f302e2e28
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-09-20 19:47:34 -0700

    Correct docs and GWT behavior of NoiseUtils.
    On GWT before, if `seed + xFloor` exceeded the min or max integer, the behavior could be different from desktop/Android/etc. where seed + xFloor would wrap via well-defined overflow semantics. Now, all terms should be promoted to `long`, which gives them well-defined behavior even on GWT. This also more appropriately credits Inigo Quilez by linking to his original work on ShaderToy.

--- src/main/java/com/github/tommyettinger/textra/utils/NoiseUtils.java
+++ src/main/java/com/github/tommyettinger/textra/utils/NoiseUtils.java
@@ -25,11 +25,17 @@ import com.badlogic.gdx.utils.NumberUtils;
 public class NoiseUtils {
     /**
      * Quilez' 1D noise, with some changes to work on the CPU. Takes a distance x and any int seed, and produces a
-     * smoothly-changing value as x goes up or down and seed stays the same. Uses a quartic curve.
+     * smoothly-changing value as x goes up or down and seed stays the same. Uses a quartic curve. You will often want
+     * to prefer {@link #octaveNoise1D(float, int)}, which uses this method, instead of directly calling noise1D(),
+     * because octaveNoise1D() looks much more natural.
      * <br>
-     * The distance ({@code x}) should be between -16384 and 1073733631 for this to return correct results.
+     * The distance ({@code x}) should be between -8192 and 1073733631 for this to return correct results.
      * Because floats incur precision loss earlier than 1073733631, the actual upper bound is lower. The limit of
      * -8192 comes from how this uses {@link MathUtils#floor(float)} internally on {@code x + x}.
+     * <br>
+     * Credit to Inigo Quilez, who posted <a href="https://www.shadertoy.com/view/3sd3Rs">this ShaderToy</a> in 2019
+     * under the MIT license. There are changes here in the way it uses a seed and gets the {@code h} value, but the
+     * core idea is the same, using a symmetrical curve to minimize the number of hashes needed per point.
      *
      * @param x    should go up and/or down steadily and by small amounts (less than 1.0, certainly)
      * @param seed should stay the same for a given curve
@@ -41,13 +47,17 @@ public class NoiseUtils {
                 rise = 1 - (MathUtils.floor(x + x) & 2);
         x -= xFloor;
         // gets a random float between -16 and 16. Magic.
-        final float h = NumberUtils.intBitsToFloat((int) ((seed + xFloor ^ 0x9E3779B97F4A7C15L) * 0xD1B54A32D192ED03L >>> 41) | 0x42000000) - 48f;
+        final float h = NumberUtils.intBitsToFloat((int) ((seed + (xFloor ^ 0x9E3779B97F4A7C15L)) * 0xD1B54A32D192ED03L >>> 41) | 0x42000000) - 48f;
         x *= x - 1f;
         return rise * x * x * h;
     }
 
     /**
-     * Just gets two octaves of {@link #noise1D(float, int)}; still has a range of -1 to 1.
+     * A more natural 1D noise that uses two octaves of {@link #noise1D(float, int)}; still has a range of -1 to 1.
+     * <br>
+     * Because this uses a higher frequency for one octave of noise1D(), the limit on x is more restricting; x should be
+     * no less than -4096 here, though there may be precision quirks for other inputs near that range, and excessively
+     * large inputs will naturally lose some precision due to how floats work.
      *
      * @param x    should go up and/or down steadily and by small amounts (less than 1.0, certainly)
      * @param seed should stay the same for a given curve
