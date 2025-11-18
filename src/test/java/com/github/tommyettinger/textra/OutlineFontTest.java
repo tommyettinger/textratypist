678f79ede72f57853ffc28fc77b84bd59957bc40
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-11-17 20:41:26 -0800

    FreeType works fine without padding changes.
    I still don't know how openSans30.fnt was created, so there's no way I could fix whatever program created it (or work around its outline problems here).

--- src/test/java/com/github/tommyettinger/textra/OutlineFontTest.java
+++ src/test/java/com/github/tommyettinger/textra/OutlineFontTest.java
@@ -5,7 +5,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Stack;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -29,15 +31,36 @@ public class OutlineFontTest extends ApplicationAdapter {
 
 //        top of the outline is cut off. font was generated with... not currently known, but
 //        the outlines have pointy corners rather than rounded ones.
+        // https://i.imgur.com/5julXro.png
 //        BitmapFont font = new BitmapFont(Gdx.files.internal("openSans30.fnt"));
 
 //        no outlines are cut off. font was generated with AngelCode BMFont with outline=1 and padding=2,2,2,2 .
 //        outlines have rounded corners.
+        // https://i.imgur.com/KBmOFPT.png
 //        BitmapFont font = new BitmapFont(Gdx.files.internal("Open-Sans-Extra-Bold-standard.fnt"));
 
 //        larger font, outline works; should roughly match the size of openSans30.fnt. Made with AngelCode
 //        BMFont again. padding is 0,0,0,0 this time, outline is still 1. spacing is still 1.
-        BitmapFont font = new BitmapFont(Gdx.files.internal("Open-Sans-ExtraBold-standard.fnt"));
+        // https://i.imgur.com/gJE61BC.png
+//        BitmapFont font = new BitmapFont(Gdx.files.internal("Open-Sans-ExtraBold-standard.fnt"));
+
+//        FreeType works too with no padding changes.
+        // https://i.imgur.com/4mbGVLo.png
+        FreeTypeFontGenerator ft = new FreeTypeFontGenerator(Gdx.files.internal("OpenSans-ExtraBold.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.borderColor = Color.BLACK;
+        parameter.color = Color.WHITE;
+        parameter.borderWidth = 1f;
+        parameter.borderStraight = true;
+        parameter.renderCount = 4;
+        parameter.characters = Gdx.files.internal("OpenSans-ExtraBold.txt").readString("UTF-8");
+        parameter.gamma = 2.2f;
+        parameter.hinting = FreeTypeFontGenerator.Hinting.Medium;
+        parameter.kerning = true;
+        parameter.size = 30;
+        parameter.minFilter = Texture.TextureFilter.Linear;
+        parameter.magFilter = Texture.TextureFilter.Linear;
+        BitmapFont font = ft.generateFont(parameter);
 
         font.setUseIntegerPositions(false);
 
