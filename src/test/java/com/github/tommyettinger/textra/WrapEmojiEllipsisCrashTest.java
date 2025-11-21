b18fa496c75058491bff1b5d526a3409d9e40a01
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-11-21 13:58:19 -0800

    Reproduce #36 ; thanks. Lucas, for the easy test.
    I put the assets into a different directory because we already have a folder called `crash/` and I didn't want crash.atlas and crash.png to get mixed up with it. I also changed the background to gray and set the texture filter to linear, which doesn't help the crash unless the ellipsis is disabled. Without the ellipsis, this works well.

---
+++ src/test/java/com/github/tommyettinger/textra/WrapEmojiEllipsisCrashTest.java
@@ -0,0 +1,85 @@
+package com.github.tommyettinger.textra;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class WrapEmojiEllipsisCrashTest extends ApplicationAdapter {
+
+  private Font font;
+  private Layout layout;
+  private SpriteBatch batch;
+
+  private ScreenViewport viewport;
+
+  public WrapEmojiEllipsisCrashTest() {
+  }
+
+  @Override
+  public void create() {
+    BitmapFont bitmapFont = new BitmapFont(Gdx.files.internal("the_works/open_sans_big.fnt"));
+    bitmapFont.setUseIntegerPositions(false);
+
+    TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("the_works/crash.atlas"));
+
+    font = new Font(bitmapFont, 0, -22, 0, -16);
+    font.setTextureFilter();
+    font.addAtlas(atlas, 0, 6, 0);
+    font.setInlineImageStretch(32 / font.cellHeight);
+    font.scaleTo(20.114286f, 13.257143f);
+
+    layout = new Layout();
+    layout.setFont(font);
+    layout.setTargetWidth(115.20001f);
+    layout.setMaxLines(4);
+    // this is definitely causing some mismatch with layout.advances ...
+    layout.setEllipsis("...");
+
+    font.markup("Testtt:\ntesting test [+emoji] haha hehe testing test [+emoji] haha hehe", layout);
+
+    batch = new SpriteBatch();
+
+    viewport = new ScreenViewport();
+    viewport.setUnitsPerPixel(0.45714286f);
+  }
+
+  @Override
+  public void render() {
+    ScreenUtils.clear(Color.GRAY);
+
+    viewport.apply();
+    batch.setProjectionMatrix(viewport.getCamera().combined);
+
+    batch.begin();
+    font.drawGlyphs(batch, layout, 0, 0);
+    batch.end();
+  }
+
+  @Override
+  public void dispose() {
+    batch.dispose();
+  }
+
+  @Override
+  public void resize(int width, int height) {
+    viewport.update(width, height, false);
+  }
+
+  public static void main(String[] args){
+    Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+    config.setTitle("Font crash test");
+    config.setWindowedMode(600, 500);
+    config.disableAudio(true);
+    config.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
+    config.useVsync(true);
+    new Lwjgl3Application(new WrapEmojiEllipsisCrashTest(), config);
+  }
+}
+
