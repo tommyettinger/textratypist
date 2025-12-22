0953a138f8c95d9d0972285ad4faf467b4bfdfd9
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-12-22 14:07:29 -0800

    Large x in NoiseUtils seems to produce 0 often.
    Large, even x, in particular. Thanks to ooooooooooooooooooooo for discovering this! It quickly tends to move away from 0 if you add small floats to x, though.

---
+++ src/test/java/com/github/tommyettinger/textra/NoiseUtilsTest.java
@@ -0,0 +1,25 @@
+package com.github.tommyettinger.textra;
+
+import com.github.tommyettinger.textra.utils.NoiseUtils;
+
+public class NoiseUtilsTest {
+    public static void main(String[] args) {
+        int seed = 11;
+        // when x is exactly a large even integer, the result seems to be 0 very often...
+        System.out.printf("x=%f, seed=%010d: %f\n", 1100.0f, seed, NoiseUtils.octaveNoise1D(1100.0f, seed));
+        System.out.printf("x=%f, seed=%010d: %f\n", 1100.1f, seed, NoiseUtils.octaveNoise1D(1100.1f, seed));
+        System.out.printf("x=%f, seed=%010d: %f\n", 1100.2f, seed, NoiseUtils.octaveNoise1D(1100.2f, seed));
+        System.out.printf("x=%f, seed=%010d: %f\n", 1100.3f, seed, NoiseUtils.octaveNoise1D(1100.3f, seed));
+        System.out.printf("x=%f, seed=%010d: %f\n", 1100.4f, seed, NoiseUtils.octaveNoise1D(1100.4f, seed));
+        System.out.printf("x=%f, seed=%010d: %f\n", 1100.5f, seed, NoiseUtils.octaveNoise1D(1100.5f, seed));
+        System.out.printf("x=%f, seed=%010d: %f\n", 1100.6f, seed, NoiseUtils.octaveNoise1D(1100.6f, seed));
+        System.out.printf("x=%f, seed=%010d: %f\n", 1100.7f, seed, NoiseUtils.octaveNoise1D(1100.7f, seed));
+        System.out.printf("x=%f, seed=%010d: %f\n", 1100.8f, seed, NoiseUtils.octaveNoise1D(1100.8f, seed));
+        System.out.printf("x=%f, seed=%010d: %f\n", 1100.9f, seed, NoiseUtils.octaveNoise1D(1100.9f, seed));
+
+        System.out.printf("x=%f, seed=%010d: %f\n", 1101.0f, seed, NoiseUtils.octaveNoise1D(1101.0f, seed));
+        System.out.printf("x=%f, seed=%010d: %f\n", 1101.1f, seed, NoiseUtils.octaveNoise1D(1101.1f, seed));
+        System.out.printf("x=%f, seed=%010d: %f\n", 1101.2f, seed, NoiseUtils.octaveNoise1D(1101.2f, seed));
+        System.out.printf("x=%f, seed=%010d: %f\n", 1101.3f, seed, NoiseUtils.octaveNoise1D(1101.3f, seed));
+    }
+}
