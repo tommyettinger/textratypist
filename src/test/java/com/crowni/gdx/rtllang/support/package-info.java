793e2a47fb8f1e856be0fbff72605d8faa29d7c5
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-02-01 22:52:47 -0800

    Move WIP Arabic code to chortle repo.
    See: https://github.com/tommyettinger/chortle
    It handles Hebrew now, too!

--- src/test/java/com/crowni/gdx/rtllang/support/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * This package is from
- * <a href="https://github.com/iibrahimbakr/libGDX-RTL-Language">iibrahimbakr's repo</a>, Apache 2.0 licensed.
- * Minor changes have been made to reduce allocation and eliminate runtime debug printing.
- */
-package com.crowni.gdx.rtllang.support;
