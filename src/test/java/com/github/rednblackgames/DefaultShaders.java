1fc1da9c8516abfb3036e7bc040e0f18054a9452
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-02-28 19:18:10 -0800

    SDF outline (w/ and w/o derivatives) shaders.

--- src/test/java/com/github/rednblackgames/DefaultShaders.java
+++ src/test/java/com/github/rednblackgames/DefaultShaders.java
@@ -90,6 +90,63 @@ public class DefaultShaders {
             + "}\n";
     public static String SDF_DERIVATIVE_ARRAY_VERTEX_SHADER = DEFAULT_ARRAY_VERTEX_SHADER;
 
+    public static String SDF_OUTLINE_ARRAY_FRAGMENT_SHADER = "#ifdef GL_ES\n" //
+            + "precision mediump float;\n" //
+            + "precision mediump int;\n" //
+            + "#endif\n" //
+            + "\n" //
+            + "uniform sampler2D u_textures[MAX_TEXTURE_UNITS];\n" //
+            + "uniform float u_smoothing;\n" //
+            + "varying vec4 v_color;\n" //
+            + "varying vec2 v_texCoords;\n" //
+            + "varying float v_texture_index;\n" //
+            + "\n" //
+            + ShaderCompiler.GET_TEXTURE_FROM_ARRAY_PLACEHOLDER + "\n"
+            + "\n" //
+            + "const float closeness = 0.0625;\n" // Between 0 and 0.5, 0 = thick outline, 0.5 = no outline
+            + "\n" //
+            + "void main() {\n" //
+            + "	   if (u_smoothing > 0.0) {\n" //
+            + "        vec4 image = getTextureFromArray(v_texCoords);\n" //
+            + "        float smoothing = 0.5 / u_smoothing;\n" //
+            + "        float outlineFactor = smoothstep(0.5 - smoothing, 0.4 * smoothing + 0.5, image.a);\n" //
+            + "        vec3 color = image.rgb * v_color.rgb * outlineFactor;\n" //
+            + "        float alpha = smoothstep(closeness, closeness + 0.1, image.a);\n" //
+            + "        gl_FragColor = vec4(color, v_color.a * alpha);\n" //
+            + "    } else {\n" //
+            + "        gl_FragColor = v_color * getTextureFromArray(v_texCoords);\n" //
+            + "    }\n" //
+            + "}\n";
+    public static String SDF_OUTLINE_ARRAY_VERTEX_SHADER = DEFAULT_ARRAY_VERTEX_SHADER;
+
+    public static String SDF_OUTLINE_DERIVATIVE_ARRAY_FRAGMENT_SHADER = "#ifdef GL_ES\n" //
+            + "#extension GL_OES_standard_derivatives : enable\n" //
+            + "precision mediump float;\n" //
+            + "precision mediump int;\n" //
+            + "#endif\n" //
+            + "\n" //
+            + "uniform sampler2D u_textures[MAX_TEXTURE_UNITS];\n" //
+            + "uniform float u_smoothing;\n" //
+            + "varying vec4 v_color;\n" //
+            + "varying vec2 v_texCoords;\n" //
+            + "varying float v_texture_index;\n" //
+            + "\n" //
+            + ShaderCompiler.GET_TEXTURE_FROM_ARRAY_PLACEHOLDER + "\n"
+            + "\n" //
+            + "void main() {\n" //
+            + "	   if (u_smoothing > 0.0) {\n" //
+            + "        vec4 image = getTextureFromArray(v_texCoords);\n" //
+            + "        float smoothing = 0.8 * length(vec2(dFdx(image.a), dFdy(image.a)));\n" //
+            + "        float outlineFactor = smoothstep(0.5 - smoothing, 0.4 * smoothing + 0.5, image.a);\n" //
+            + "        vec3 color = image.rgb * v_color.rgb * outlineFactor;\n" //
+            + "        float alpha = smoothstep(closeness, closeness + 0.66 / u_smoothing, image.a);\n" //
+            + "        gl_FragColor = vec4(color, v_color.a * alpha);\n" //
+            + "    } else {\n" //
+            + "        gl_FragColor = v_color * getTextureFromArray(v_texCoords);\n" //
+            + "    }\n" //
+            + "}\n";
+    public static String SDF_OUTLINE_DERIVATIVE_ARRAY_VERTEX_SHADER = DEFAULT_ARRAY_VERTEX_SHADER;
+
     public static String MSDF_ARRAY_FRAGMENT_SHADER = "#ifdef GL_ES\n" //
             + "precision mediump float;\n" //
             + "precision mediump int;\n" //
