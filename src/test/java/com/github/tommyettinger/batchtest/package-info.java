99df15ee84049f6aa5b8d98e14697a2bf1c7531b
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-04-27 14:49:08 -0700

    Test out the new batch package; it work with...
    ... vanilla scene2d.ui just fine.

---
+++ src/test/java/com/github/tommyettinger/batchtest/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Not meant to be copied into sources like com.github.tommyettinger.batch . This could be copied into tests, but
+ * it doesn't need to be.
+ */
+package com.github.tommyettinger.batchtest;
