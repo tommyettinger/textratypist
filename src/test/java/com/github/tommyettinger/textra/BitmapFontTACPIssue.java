d051ffe4ebb9cb4c73b32b76796598fde3a14e29
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-08-15 19:40:09 -0700

    Update to -SNAPSHOT, isolate TACP flush bug.

---
+++ src/test/java/com/github/tommyettinger/textra/BitmapFontTACPIssue.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2024 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.tommyettinger.textra;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.PolygonSpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class BitmapFontTACPIssue extends ApplicationAdapter {
+
+  private Stage stage;
+
+  public BitmapFontTACPIssue() {
+  }
+
+  @Override
+  public void create() {
+    // TextureArrayCpuPolygonSpriteBatch is an alternative to SpriteBatch that does some things better.
+    TextureArrayCpuPolygonSpriteBatch batch = new TextureArrayCpuPolygonSpriteBatch(1000); // fails, debugging
+    // When using a TextureArray batch, you need to call this line before using anything from KnownFonts.
+    // Usually this line goes right after creating a TextureArrayCpuPolygonSpriteBatch, at the start of create() .
+    TextureArrayShaders.initializeTextureArrayShaders();
+
+    // works fine.
+//    PolygonSpriteBatch batch = new PolygonSpriteBatch(1000);
+    stage = new Stage(new ScreenViewport(), batch);
+    BitmapFont font = new BitmapFont();
+    font.setUseIntegerPositions(false);
+    Label.LabelStyle style = new Label.LabelStyle(font, Color.WHITE);
+    String text = (
+            "To pay homage to the great GBTK and a celebration of reaching the tenth one, I have created this masterpiece to reflect upon the happy times we've had together.\n" +
+            "\n" +
+            "Prepare yourself for a sensuous journey, in which you will have to grab the games from the previous nine GBTKs and categorise them into the appropriate jam. Once all games have been categorised, you shall be automatically whisked away to view how well you did without a chance to rethink your final placement.\n" +
+            "\n" +
+            "I am legally required to inform you that this tribute has been linked with hearing loss, seizures, and birth defects."
+    ).replace(" ", "  ");
+    Label label = new Label(text, style);
+    label.setWrap(true);
+
+    // Making sure setBounds() works the same for Label and TypingLabel.
+    label.setBounds(25, 25, 420, 400);
+
+    stage.addActor(label);
+  }
+
+  @Override
+  public void render() {
+    ScreenUtils.clear(Color.BLACK);
+    stage.act();
+    stage.getViewport().apply(true);
+    stage.draw();
+  }
+
+  @Override
+  public void resize(int width, int height) {
+    stage.getViewport().update(width, height);
+  }
+
+  public static void main(String[] args){
+    Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+    config.setTitle("TextraLabel vs. Label test");
+    config.setWindowedMode(600, 500);
+    config.disableAudio(true);
+    config.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate);
+    config.useVsync(true);
+    new Lwjgl3Application(new BitmapFontTACPIssue(), config);
+  }
+}
