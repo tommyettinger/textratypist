f076df801594e20e24e3130b9f14938a12a7f536
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-09-22 16:10:24 -0700

    Using JsonBiter is a little faster than GDX Json.
    It's likely the Lzma decompression is taking a constant chunk of time for both.

--- src/test/java/com/github/tommyettinger/textra/StructureBiterTest.java
+++ src/test/java/com/github/tommyettinger/textra/StructureBiterTest.java
@@ -6,27 +6,30 @@ import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonWriter;
 import com.badlogic.gdx.utils.StreamUtils;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.compression.Lzma;
+import com.github.tommyettinger.jsonbiter.JsonIterator;
+import com.github.tommyettinger.jsonbiter.spi.Config;
 import com.github.tommyettinger.textra.json.FontData;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
 
 /**
- * Loading Maple-Mono-standard.json.lzma took 0.3275592 seconds.
+ * Loading Maple-Mono-standard.json.lzma took 0.2290812 seconds.
  */
-public class StructureLoadTest {
+public class StructureBiterTest {
     public static void main(String[] args) throws IOException {
         Gdx.files = new Lwjgl3Files();
         long startTime = System.nanoTime();
-        //        FileHandle jsonHandle = Gdx.files.internal("Birdland-Aeroplane-standard.json.lzma");
-//        FileHandle jsonHandle = Gdx.files.internal("Crete-Round-sdf.json.lzma");
+
+//        FileHandle jsonHandle = Gdx.files.internal("Birdland-Aeroplane-standard.json.lzma");
         FileHandle jsonHandle = Gdx.files.internal("Maple-Mono-standard.json.lzma");
         BufferedInputStream bais = jsonHandle.read(4096);
         StreamUtils.OptimizedByteArrayOutputStream baos = new StreamUtils.OptimizedByteArrayOutputStream(4096);
         Lzma.decompress(bais, baos);
-        Json json = new Json(JsonWriter.OutputType.json);
-        FontData fd = json.fromJson(FontData.class, baos.toString("UTF-8"));
+        Config cfg = new Config.Builder().omitDefaultValue(true).escapeUnicode(false).build();
+        FontData fd = JsonIterator.deserialize(cfg, baos.toByteArray(), FontData.class);
 
         System.out.println("Loading " + jsonHandle.name() + " took " + (System.nanoTime() - startTime) * 1E-9 + " seconds.");
         System.out.println();
